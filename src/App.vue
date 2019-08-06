c665b058938704adb2e54cbe7ae8f7713a4749ff
chrisfauries <45318711+chrisfauries@users.noreply.github.com>
2019-08-06 16:08:23 -0500

    CSS

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
-    <Board v-bind:size="size" />
+  <div id="app" v-bind:class="{gameOver : gameOver}">
+    <Board v-bind:size="size" @gameOver="shake" />
   </div>
 </template>
 
 <script>
-import Board from './components/Board.vue'
+import Board from './components/Board.vue';
 
 export default {
   name: 'app',
@@ -15,17 +15,44 @@ export default {
   data: function() {
     return {
       size : {
-        width : 10,
-        height : 10,
-        mines : 20
-      }
+        width : 15,
+        height : 15,
+        mines : 40
+      },
+      gameOver : false
+    }
+  },
+
+  methods : {
+    shake : function(value) {
+      this.gameOver = true;
     }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
+#app.gameOver {
+  animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;
+  transform: translate3d(0, 0, 0);
+  backface-visibility: hidden;
+  perspective: 1000px;
+}
+@keyframes shake {
+  10%, 90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+  
+  20%, 80% {
+    transform: translate3d(2px, 0, 0);
+  }
 
+  30%, 50%, 70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%, 60% {
+    transform: translate3d(4px, 0, 0);
+  }
 }
 </style>
--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,12 +2,30 @@
   <div class="container">
     <div class="row" v-bind:key="index" v-for="(row,index) in selects">
       <div
+        @contextmenu.prevent="rClick(index, jndex)"
         v-on:click="clicked(index, jndex)"
-        class="cell"
         v-bind:key="jndex"
         v-for="(cell, jndex) in row"
-        v-bind:class="{clicked : cell}"
-      >{{mines[index][jndex]}}</div>
+        class="cell scene scene--card"
+      >
+        <div class="card" v-bind:class="{isFlipped : cell}">
+          <div class="card__face card__face--front">
+            <img
+              v-if="flags[index][jndex]"
+              src="../assets/redflag.png"
+              alt="flag"
+              height="30"
+              width="30"
+            />
+          </div>
+          <div class="card__face card__face--back">
+            <div v-if="mines[index][jndex] === -1">
+              <img src="../assets/landmine.png" alt="MINE!" height="30" width="30" />
+            </div>
+            <div v-else>{{mines[index][jndex]}}</div>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="gameOver" v-bind:class="{show : isGameOver}">GAME OVER</div>
   </div>
@@ -15,22 +33,24 @@
 
 <script>
 import Logic from "../assets/logic.js";
-// console.log(size)
+import uuidv4 from "uuid/v4";
 export default {
-  props: ['size'],
+  props: ["size"],
   data: function() {
-		const { height, width, mines } = this.size;
-		console.log()
+    const { height, width, mines } = this.size;
     return {
       selects: Logic.selectBoard(height, width),
       mines: Logic.mineBoard(height, width, mines),
+      flags: Logic.selectBoard(height, width),
       isGameOver: false
     };
   },
 
   methods: {
     clicked: function(index, jndex) {
-			console.log(this.size)
+      if (this.isGameOver) {
+        return;
+      }
       let value = Logic.updateSelectionBoard(
         this.mines,
         this.selects,
@@ -39,8 +59,21 @@ export default {
         this
       );
       if (!value) {
+				this.$set(this.selects[index], jndex, true);
+				this.$emit('gameOver', true);
         this.gameOver();
       }
+    },
+
+    id: function() {
+      return uuidv4();
+    },
+
+    rClick: function(X, Y) {
+      if (this.isGameOver) {
+        return;
+      }
+      this.$set(this.flags[X], Y, true);
     },
 
     gameOver: function() {
@@ -52,14 +85,19 @@ export default {
 
 <style lang="scss" scoped>
 .container {
+  margin: 0 auto;
+  width: 750px;
+  height: 800px;
 }
+
 .row {
+  height: 50px;
 }
 
 .cell {
   display: inline-block;
-  width: 75px;
-  height: 75px;
+  width: 50px;
+  height: 50px;
   background-color: #eee;
   box-sizing: border-box;
   border: 1px solid black;
@@ -77,5 +115,40 @@ export default {
 
 .show {
   display: block;
+}
+
+.card {
+  cursor: url(http://cur.cursors-4u.net/others/oth-3/oth213.cur), auto;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  transform-style: preserve-3d;
+  transform-origin: center right;
+  transition: transform 1s;
+}
+
+.card.isFlipped {
+  transform: translateX(-100%) rotateY(-180deg);
+}
+
+.card__face {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  line-height: 50px;
+  color: white;
+  text-align: center;
+  font-weight: bold;
+  font-size: 30px;
+  backface-visibility: hidden;
+}
+
+.card__face--front {
+  background: green;
+}
+
+.card__face--back {
+  background: #2b1d0e;
+  transform: rotateY(180deg);
 }
 </style>
